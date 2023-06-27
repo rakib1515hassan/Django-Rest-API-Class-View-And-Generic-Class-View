90ce666b98daa66bfaa69f71db8c73a0130ed88f
Md Rakib Hassan <rakib1515hassan@gmail.comm>
2023-06-27 13:46:09 +0600

    Model View Set Add

--- ViewSets/urls.py
+++ ViewSets/urls.py
@@ -3,6 +3,10 @@ from django.urls import path, include
 from ViewSets.views._ViewSets import (
     TeacherViewSet
 )
+from ViewSets.views._ModelViewSets import (
+    TeacherModelViewSet,
+    TeacherReadOnlyModelViewSet,
+)
 
 from rest_framework.routers import DefaultRouter
 
@@ -12,10 +16,18 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 
 # Router Registration StudentViewSet With Router
+# ViewSet
 router.register('teacher-view-set', TeacherViewSet, basename='Teacher_ViewSets')
+# ModelViewSet
+router.register('teacher-model-view-set', TeacherModelViewSet, basename='Teacher_Model_ViewSet')
+# ReadOnlyModelViewSet
+router.register('teacher-read-model-view-set', TeacherReadOnlyModelViewSet, basename='Read_Only_Model_ViewSet')
 
 
+## http://127.0.0.1:8000/view-sets/viewsets/
 ## URL = ( http://127.0.0.1:8000/view-sets/viewsets/teacher-view-set/ )
+## URL = ( http://127.0.0.1:8000/view-sets/viewsets/teacher-model-view-set/ )
+## URL = ( http://127.0.0.1:8000/view-sets/viewsets/teacher-read-model-view-set/ )
 urlpatterns = [
     path('viewsets/', include(router.urls)),
 ]
--- ViewSets/views/_ModelViewSets.py
+++ ViewSets/views/_ModelViewSets.py
@@ -12,7 +12,20 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
-# Create your views here.
-# class TeacherViewSet(ViewSet):
-#     queryset = Teacher.objects.all()
-#     serializer_class = TeacherSerializer
+## Create your views here.
+
+## URL = ( http://127.0.0.1:8000/view-sets/viewsets/teacher-model-view-set/ )   ## দুইটি এক সাথে কাজ করবে।
+## URL = ( http://127.0.0.1:8000/view-sets/viewsets/teacher-model-view-set/25/ ) ## এখানে শেষে id বসবে।
+class TeacherModelViewSet(ModelViewSet):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+
+
+
+## URL = ( http://127.0.0.1:8000/view-sets/viewsets/teacher-read-model-view-set/ )
+## URL = ( http://127.0.0.1:8000/view-sets/viewsets/teacher-read-model-view-set/23/ ) ## এখানে শেষে id বসবে।
+## এর মাধ্যমে আমরা শুধু GET Method access করতে পারবো।
+class TeacherReadOnlyModelViewSet(ReadOnlyModelViewSet):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
